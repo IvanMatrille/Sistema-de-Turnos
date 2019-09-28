3de6c008830d0526b305734584adf16c124e1e20
Programador <jdeleon00123@gmail.com>
2019-09-28 11:03:04 -0400

    agregando font faltante y arreglo del url para el uso del fontawesome de manera local

--- listadoCitas.php
+++ listadoCitas.php
@@ -6,8 +6,7 @@
     <title>Cita</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-   <!--<link rel="stylesheet" href="css/all.css" > -->
+    <link rel="stylesheet" href="css/all.css">  
    
     <link rel="stylesheet" type="text/css" href="css/principal.css" media="screen" />
 </head>
